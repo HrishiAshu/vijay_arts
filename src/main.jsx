c7f09536d4df51a90208f7479fff7926d39dffc1
Hrishikeshsonawane <hrishikeshsonawane9@gmail.com>
2024-05-27 01:28:53 +0530

    resolved redux error

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import AppStore from './Store/AppStore.js'
+import store from './Store/AppStore.js'
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <Provider value={AppStore}>
-        <App />
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+
     </Provider>
 )

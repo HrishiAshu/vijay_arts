5999cb9951769971c3922013a7d6f070a626b9c1
shreyash bansode <shreyashbansode1@gmail.com>
2024-05-26 23:59:01 +0530

    create basic folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Login from '../src/Auth/Login'
+import Register from './Auth/Register'
 
 const App = () => {
   return (
-    <div>
-      hi saapo
-    </div>
+    <Routes>
+      <Route exact path='/' element={<Login />} />
+      <Route exact path='/user/register' element={<Register />} />
+    </Routes>
+
   )
 }
 
